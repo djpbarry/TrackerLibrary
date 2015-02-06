130ba9b9017781fc0cc230e3261578eb716ddfbf
David J Barry <davejpbarry@gmail.com>
2015-02-06 11:27:03 +0000

    Modified TailFitter to fit a Gaussian-convolved exponentially modified Gaussian function

--- src/ParticleTracking/TailFitter.java
+++ src/ParticleTracking/TailFitter.java
@@ -27,8 +27,9 @@ import ij.process.ImageProcessor;
 import ij.process.ImageStatistics;
 import java.awt.Rectangle;
 import java.io.File;
-import java.util.Random;
+import org.apache.commons.math3.analysis.function.Gaussian;
 import org.apache.commons.math3.special.Erf;
+import org.apache.commons.math3.util.MathArrays;
 
 /**
  *
@@ -40,61 +41,61 @@ public class TailFitter extends IsoGaussianFitter {
     private static double sigmaEst = 1.06;
     double sqrt2 = Math.pow(2.0, 0.5);
 
-//    public static void main(String args[]) {
-////        Random r = new Random();
-//        File directory = Utilities.getFolder(null, "Select input folder", true);
-//        File files[] = directory.listFiles();
-//        int dirSize = files.length;
-//        ImagePlus temp = IJ.openImage(files[0].getAbsolutePath());
-//        ImageProcessor tempIP = temp.getProcessor();
-//        int stackwidth = tempIP.getWidth();
-//        int stackheight = tempIP.getHeight();
-//        temp.close();
-//        System.out.println(directory);
-////        for (int i = 0; i < 100; i++) {
-////            System.out.print(i);
-//            ImageStack stack = new ImageStack(stackwidth, stackheight);
-//            for (int j = 0; j < dirSize; j++) {
-////                int fileindex = r.nextInt(dirSize);
-////                ImagePlus imp = IJ.openImage(files[fileindex].getAbsolutePath());
-//            ImagePlus imp = IJ.openImage(files[j].getAbsolutePath());
-//                stack.addSlice(imp.getProcessor().duplicate());
-//                imp.close();
-//            }
-//            ZProjector zproj = new ZProjector(new ImagePlus("", stack));
-//            zproj.setMethod(ZProjector.AVG_METHOD);
-//            zproj.doProjection();
-//            ImageProcessor stackAverage = zproj.getProjection().getProcessor();
-//            ImageStatistics stats = stackAverage.getStatistics();
-//            double max = stats.max;
-//            stackAverage.multiply(1.0 / max);
-//            Rectangle cropRoi = new Rectangle(0, 6, stackAverage.getWidth() - 2, stackAverage.getHeight() - 12);
-//            stackAverage.setRoi(cropRoi);
-//            stackAverage = stackAverage.crop();
-//            int width = stackAverage.getWidth();
-//            int height = stackAverage.getHeight();
-//            double xVals[] = new double[width];
-//            double yVals[] = new double[height];
-//            double zVals[][] = new double[width][height];
-//            for (int y = 0; y < height; y++) {
-//                yVals[y] = y * spatialRes;
-//                for (int x = 0; x < width; x++) {
-//                    xVals[x] = x * spatialRes;
-//                    zVals[x][y] = stackAverage.getPixelValue(x, y);
-//                }
-//            }
-//            TailFitter tf = new TailFitter(xVals, yVals, zVals);
-//            tf.doFit(TailFitter.sigmaEst);
-//            tf.findPeak(Analyse_Movie.TRACK_OFFSET, 2.0 - 6.0 * spatialRes);
-////            IJ.saveAs((new ImagePlus("", stackAverage)), "TIFF", "C:\\Users\\barry05\\Desktop\\SuperRes Actin Tails\\Averages\\" + i);
-////        }
-//        tf.printImage();
-//        System.exit(0);
-//    }
+    public static void main(String args[]) {
+//        Random r = new Random();
+        File directory = Utilities.getFolder(null, "Select input folder", true);
+        File files[] = directory.listFiles();
+        int dirSize = files.length;
+        ImagePlus temp = IJ.openImage(files[0].getAbsolutePath());
+        ImageProcessor tempIP = temp.getProcessor();
+        int stackwidth = tempIP.getWidth();
+        int stackheight = tempIP.getHeight();
+        temp.close();
+        System.out.println(directory);
+//        for (int i = 0; i < 100; i++) {
+//            System.out.print(i);
+        ImageStack stack = new ImageStack(stackwidth, stackheight);
+        for (int j = 0; j < dirSize; j++) {
+//                int fileindex = r.nextInt(dirSize);
+//                ImagePlus imp = IJ.openImage(files[fileindex].getAbsolutePath());
+            ImagePlus imp = IJ.openImage(files[j].getAbsolutePath());
+            stack.addSlice(imp.getProcessor().duplicate());
+            imp.close();
+        }
+        ZProjector zproj = new ZProjector(new ImagePlus("", stack));
+        zproj.setMethod(ZProjector.AVG_METHOD);
+        zproj.doProjection();
+        ImageProcessor stackAverage = zproj.getProjection().getProcessor();
+        ImageStatistics stats = stackAverage.getStatistics();
+        double max = stats.max;
+        stackAverage.multiply(1.0 / max);
+        Rectangle cropRoi = new Rectangle(0, 14, stackAverage.getWidth() - 4, 1);
+        stackAverage.setRoi(cropRoi);
+        stackAverage = stackAverage.crop();
+        int width = stackAverage.getWidth();
+        int height = stackAverage.getHeight();
+        double xVals[] = new double[width];
+        double yVals[] = new double[height];
+        double zVals[][] = new double[width][height];
+        for (int y = 0; y < height; y++) {
+            yVals[y] = y * spatialRes;
+            for (int x = 0; x < width; x++) {
+                xVals[x] = x * spatialRes;
+                zVals[x][y] = stackAverage.getPixelValue(x, y);
+            }
+        }
+        TailFitter tf = new TailFitter(xVals, yVals, zVals);
+        tf.doFit(TailFitter.sigmaEst);
+        tf.findPeak(Analyse_Movie.TRACK_OFFSET, 2.0 - 6.0 * spatialRes);
+//        IJ.saveAs((new ImagePlus("", stackAverage)), "TIFF", "C:\\Users\\barry05\\Desktop\\SuperRes Actin Tails\\Average.tif");
+//        }
+        tf.printImage();
+        System.exit(0);
+    }
 
     public TailFitter(double[] xVals, double[] yVals, double[][] zVals) {
         super();
-        numParams = 8;
+        numParams = 5;
         this.xData = xVals;
         this.yData = yVals;
         this.zData = new double[xData.length * yData.length];
@@ -127,75 +128,104 @@ public class TailFitter extends IsoGaussianFitter {
         maxIter = IterFactor * numParams * numParams;  // Where does this estimate come from?
         restarts = defaultRestarts;
         nRestarts = 0;
-        simp[0][0] = 0.14 * xData.length * spatialRes;
-        simp[0][1] = 0.23;
-        simp[0][2] = 0.38 * xData.length * spatialRes;
-        simp[0][3] = 1.18;
-        simp[0][4] = 0.5 * yData.length * spatialRes;
-        simp[0][5] = 0.18;
-        simp[0][6] = 0.53;
-        simp[0][7] = 0.28;
+        simp[0][0] = 1.0; //lambda
+        simp[0][1] = 0.14 * xData.length * spatialRes; //mu
+        simp[0][2] = 0.3; //sigma
+        simp[0][3] = 0.5; //nu
+        simp[0][4] = 0.4; //noise
 
         return true;
     }
 
-    public double evaluate(double[] p, double x, double y) {
+    public double evaluate(double[] p, double tailval, double x, double y) {
         if (p == null) {
             return Double.NaN;
         }
-        double z = (x - p[0]) / (p[1] * sqrt2);
-        double w = (x - p[2]) / (p[3] * sqrt2);
-        double v = Math.pow((y - p[4]) / (p[5] * sqrt2), 2.0);
+        double v = Math.pow((y - p[3]) / (sigmaEst * sqrt2), 2.0);
 
-        return p[6] + p[7] * (Erf.erf(z) - Erf.erf(w)) * Math.exp(-0.5 * v);
+        return p[4] * tailval * Math.exp(-0.5 * v);
+    }
+
+    protected boolean sumResiduals(double[] x) {
+        if (x == null) {
+            return false;
+        }
+        /*
+         * x[numParams] = sumResiduals(x, xData, yData, zData); return true;
+         */
+        double e;
+        x[numParams] = 0.0;
+        double tail1d[] = buildTail(x, 10);
+        for (int i = 0; i < xData.length; i++) {
+            for (int j = 0; j < yData.length; j++) {
+                e = evaluate(x, tail1d[i + xData.length / 2], xData[i], yData[j]) - zData[j * xData.length + i];
+                x[numParams] = x[numParams] + (e * e);
+            }
+        }
+        return true;
+    }
+
+    double[] buildTail(double[] p, int padding) {
+        double[] emg = new double[xData.length+padding];
+        Gaussian gauss = new Gaussian(emg.length / 2.0, sigmaEst);
+        double[] gaussian = new double[emg.length];
+        for (int i = 0; i < emg.length; i++) {
+            gaussian[i] = gauss.value(i);
+            double a = 0.5 * p[0] * (2.0 * p[1] + p[0] * p[2] * p[2] - 2.0 * xData[i]);
+            double b = (p[1] + p[0] * p[2] * p[2] - xData[i]) / (sqrt2 * p[2]);
+
+            emg[i] = p[3] * p[0] * Math.exp(a) * Erf.erfc(b) + p[4];
+        }
+        return MathArrays.convolve(emg, gaussian);
     }
 
     public void findPeak(double xOffset, double yOffset) {
         double params[] = getParams();
 
-        double x1 = params[0];
-        double x12 = x1 * x1;
-        double s1 = params[1];
-        double s12 = s1 * s1;
-        double x2 = params[2];
-        double x22 = x2 * x2;
-        double s2 = params[3];
-        double s22 = s2 * s2;
-        double yCentre = params[4] - yOffset;
-
-        double c = 2.0 * s12 * s22 * (Math.log(s2) - Math.log(s1));
-        double d = c + s12 * x22 - s22 * x12;
-        double b = 2.0 * (s22 * x1 - s12 * x2);
-        double a = s12 - s22;
-
-        double root1 = (-b + Math.sqrt(b * b - 4.0 * a * d)) / (2.0 * a);
-        double root2 = (-b - Math.sqrt(b * b - 4.0 * a * d)) / (2.0 * a);
-        double xCentre;
-        if (root1 < xOffset) {
-            xCentre = root2 - xOffset;
-        } else {
-            xCentre = root1 - xOffset;
-        }
-
+//        double x1 = params[0];
+//        double x12 = x1 * x1;
+//        double s1 = params[1];
+//        double s12 = s1 * s1;
+//        double x2 = params[2];
+//        double x22 = x2 * x2;
+//        double s2 = params[3];
+//        double s22 = s2 * s2;
+//        double yCentre = params[4] - yOffset;
+//
+//        double c = 2.0 * s12 * s22 * (Math.log(s2) - Math.log(s1));
+//        double d = c + s12 * x22 - s22 * x12;
+//        double b = 2.0 * (s22 * x1 - s12 * x2);
+//        double a = s12 - s22;
+//
+//        double root1 = (-b + Math.sqrt(b * b - 4.0 * a * d)) / (2.0 * a);
+//        double root2 = (-b - Math.sqrt(b * b - 4.0 * a * d)) / (2.0 * a);
+//        double xCentre;
+//        if (root1 < xOffset) {
+//            xCentre = root2 - xOffset;
+//        } else {
+//            xCentre = root1 - xOffset;
+//        }
         System.out.print(" p[0]: " + params[0]
                 + " p[1]: " + params[1]
                 + " p[2]: " + params[2]
                 + " p[3]: " + params[3]
                 + " p[4]: " + params[4]
-                + " p[5]: " + params[5]
-                + " p[6]: " + params[6]
-                + " p[7]: " + params[7]);
-        System.out.print(" x0: " + xCentre + " y0: " + yCentre + " R^2: " + params[numParams]);
+                + " p[5]: " + params[5]);
+//                + " p[6]: " + params[6]);
+//                + " p[7]: " + params[7]);
+//        System.out.print(" x0: " + xCentre + " y0: " + yCentre + " R^2: " + params[numParams]);
         System.out.println();
     }
 
     void printImage() {
-        FloatProcessor output = new FloatProcessor(xData.length, yData.length);
+        FloatProcessor deconvolved = new FloatProcessor(xData.length, yData.length);
+        FloatProcessor convolved = new FloatProcessor(xData.length, yData.length);
+        double tail1d[] = buildTail(simp[best], 10);
         for (int y = 0; y < yData.length; y++) {
             for (int x = 0; x < xData.length; x++) {
-                output.putPixelValue(x, y, evaluate(simp[best], xData[x], yData[y]));
+                deconvolved.putPixelValue(x, y, evaluate(simp[best], tail1d[x + xData.length / 2], xData[x], yData[y]));
             }
         }
-        IJ.saveAs((new ImagePlus("", output)), "text image", "C:\\Users\\barry05\\Desktop\\Average.txt");
+        IJ.saveAs((new ImagePlus("", deconvolved)), "text image", "C:\\Users\\barry05\\Desktop\\SuperRes Actin Tails\\Deconvolved.txt");
     }
 }
